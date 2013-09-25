71bf033bb60f09b779f5713f7580dfc7ec8fc40e
Sérgio Vieira <sergiosvieira@gmail.com>
2013-09-25 13:07:17 -0300

    Initial Commit

---
+++ FastForm/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FastForm
+//
+//  Created by Paulo Pinheiro on 9/25/13.
+//  Copyright (c) 2013 Bravo Inovação. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FastForm/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FastForm
+//
+//  Created by Paulo Pinheiro on 9/25/13.
+//  Copyright (c) 2013 Bravo Inovação. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
